77adc3077fc27956418d908d33d97e3d2d75b2bb
Nathaniel Reeder <n.ash.reeder@gmail.com>
2023-04-25 14:43:30 -0600

    Part 1 done

---
+++ melons.py
@@ -0,0 +1,48 @@
+import csv
+
+class Melon():
+
+    def __init__(self, melon_id, common_name, price, image_url, color, seedless):
+        self.melon_id = melon_id
+        self.common_name = common_name
+        self.price = price
+        self.image_url = image_url
+        self.color = color
+        self.seedless = seedless
+
+    def __repr__(self):
+        '''Convenience method to show information about melon in console'''
+
+        return (
+            f"<Melon: {self.melon_id}, {self.common_name}>"
+        )
+
+    def price_str(self):
+        '''Return price formatted as a string $x.xx'''
+
+        return f"${self.price:.2f}"
+
+melon_dict = {}
+
+with open('melons.csv', 'r', newline='\n') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for item in reader:
+        melon_id = item['melon_id']
+        melon = Melon(
+            melon_id, 
+            item['common_name'],
+            float(item['price']),
+            item['image_url'],
+            item['color'],
+            eval(item['seedless'])
+        )
+        melon_dict[melon_id] = melon
+
+def get_melon_by_id(id):
+    return melon_dict[id]
+    
+
+def get_melon_list():
+    return list(melon_dict.values())
+
+print(get_melon_list())
---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, redirect, flash, request
+import jinja2
+
+app = Flask(__name__)
+app.jinja_env.undefined = jinja2.StrictUndefined # for debugging purposes
+
+### Flask Routes go here. ###
+@app.route('/')
+def homepage():
+    return render_template("base.html")
+
+@app.route('/melons')
+def all_melons():
+    '''Return a page showing all melons available for purchase'''
+    return render_template("all_melons.html")
+
+@app.route('/melon/<melon_id>')
+def melon_details(melon_id):
+    '''Return a page showing all info about a melon. Also provide a button to buy that melon.'''
+    return render_template("melon_details.html")
+
+@app.route('/add_to_cart/<melon_id>')
+def add_to_cart(melon_id):
+    '''Add a melon to the shopping cart'''
+    return f"{melon_id} add to cart"
+
+@app.route('/cart')
+def show_shopping_cart():
+    '''display contents of shopping cart.'''
+    return render_template('cart.html')
+
+if __name__ == "__main__":
+    app.env = 'development'
+    app.run(debug = True, port = 8000, host = 'localhost')
