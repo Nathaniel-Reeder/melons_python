105e27066c15ea54ca275135593c363e5e934481
Nathaniel Reeder <n.ash.reeder@gmail.com>
2023-04-27 20:31:37 -0600

    Part 3 completed

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, redirect, flash, request
+from flask import Flask, render_template, redirect, flash, request, session
 import jinja2
 from melons import get_melon_list, get_melon_by_id
 
 app = Flask(__name__)
+app.secret_key = 'dev'
 app.jinja_env.undefined = jinja2.StrictUndefined # for debugging purposes
 
 ### Flask Routes go here. ###
@@ -26,12 +27,42 @@ def melon_details(melon_id):
 @app.route('/add_to_cart/<melon_id>')
 def add_to_cart(melon_id):
     '''Add a melon to the shopping cart'''
-    return f"{melon_id} added to cart"
+    if 'cart' not in session:
+        session['cart'] = {}
+    cart = session['cart'] #store cart in local variable
+    
+    cart[melon_id] = cart.get(melon_id, 0) + 1
+    session.modified = True
+    flash(f"{melon_id} added to cart")
+    print(cart)
+    
+    return redirect('/cart')
 
 @app.route('/cart')
 def show_shopping_cart():
     '''display contents of shopping cart.'''
-    return render_template('cart.html')
+    order_total = 0
+    cart_melons = []
+    
+    #Get the cart dictionary from the session (or an empty one if none exists yet)
+    cart = session.get("cart", {})
+    
+    for melon_id, qty in session['cart'].items():
+        melon = get_melon_by_id(melon_id)
+        melon_type_total = melon.price * qty
+        order_total += melon_type_total
+        melon.quantity = qty
+        melon.type_total = melon_type_total
+        
+        cart_melons.append(melon)
+    
+    return render_template('cart.html', cart_melons=cart_melons, order_total=order_total)
+
+@app.route('/empty-cart')
+def empty_cart():
+    session['cart'] = {}
+    
+    return redirect('/cart')
 
 if __name__ == "__main__":
     app.env = 'development'
