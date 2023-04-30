cf9d3fd86178e2bbe4898614109d54e791392f05
Nathaniel Reeder <n.ash.reeder@gmail.com>
2023-04-30 13:04:51 -0600

    Project complete.

--- forms.py
+++ forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, validators
+from wtforms import PasswordField, StringField, IntegerField, validators
 
 class LoginForm(FlaskForm):
     username = StringField('Username', [validators.InputRequired()])
     password = PasswordField('Password', [validators.InputRequired()])
-    
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, flash, request, session
 import jinja2
 from melons import get_melon_list, get_melon_by_id
 from forms import LoginForm
+from customers import get_by_username
 
 app = Flask(__name__)
 app.secret_key = 'dev'
@@ -16,7 +17,29 @@ def homepage():
 def login():
     '''Log user into site.'''
     form = LoginForm(request.form)
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+    
+        customer = get_by_username(username)
+        
+        if not customer or customer['password'] != password:
+            flash("Invalid username or password")
+            return redirect('/login')
+        
+        session['username'] = customer['username']
+        flash('Logged in.')
+        return redirect('/melons')    
+    
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    '''Log user out'''
+    del session['username']
+    flash('User logged out')
+    return redirect('/login')
+    
 
 @app.route('/melons')
 def all_melons():
@@ -29,11 +52,17 @@ def all_melons():
 def melon_details(melon_id):
     '''Return a page showing all info about a melon. Also provide a button to buy that melon.'''
     melon = get_melon_by_id(melon_id)
+        
     return render_template("melon_details.html", melon=melon)
 
 @app.route('/add_to_cart/<melon_id>')
 def add_to_cart(melon_id):
     '''Add a melon to the shopping cart'''
+    
+    #Check if user is logged in
+    if 'username' not in session:
+        return redirect('/login')
+    
     if 'cart' not in session:
         session['cart'] = {}
     cart = session['cart'] #store cart in local variable
@@ -48,6 +77,11 @@ def add_to_cart(melon_id):
 @app.route('/cart')
 def show_shopping_cart():
     '''display contents of shopping cart.'''
+    
+    #Check if user is logged in
+    if 'username' not in session:
+        return redirect('/login')
+    
     order_total = 0
     cart_melons = []
     
@@ -70,6 +104,10 @@ def empty_cart():
     session['cart'] = {}
     
     return redirect('/cart')
+
+@app.errorhandler(404)
+def error_404(error):
+    return render_template('404.html')
 
 if __name__ == "__main__":
     app.env = 'development'
